51d962043856dcd0e7644c959fed74bb3e212c2f
Carlos Aznarán Laos <caznaranl@uni.pe>
2022-05-03 15:16:06 -0500

    Update

---
+++ src/2022-05-02/demo/student.hh
@@ -0,0 +1,23 @@
+#pragma once // prevents multiple definitions
+
+// Test for exercise 1
+auto result_1a = 0; // TODO: Complete
+auto result_1b = 0; // TODO: Complete
+auto result_1c = 0; // TODO: Complete
+
+// Test for exercise 2
+auto result_2a = 0; // TODO: Complete
+auto result_2b = 0; // TODO: Complete
+
+// Test for exercise 3
+float x = 6.5, y = 3.8;
+
+auto expr1 = [](float x, float y)
+{
+  return 0; // TODO: Complete
+};
+
+auto expr2 = [](float x, float y)
+{
+  return 0; // TODO: Complete
+};
--- src/2022-05-02/demo/test.cc
+++ src/2022-05-02/demo/test.cc
@@ -2,9 +2,27 @@
 
 #include <catch2/catch.hpp>
 
-auto x = 1.1 + 0.1 + 0.1;
+#include "student.hh"
 
-TEST_CASE("Comparing values")
+TEST_CASE("Comparing values from first")
 {
-  REQUIRE(x == Approx(1.3));
+  REQUIRE(result_1a == Approx(320.79));
+
+  REQUIRE(result_1b == Approx(-67.342));
+
+  REQUIRE(result_1c == Approx(23.816));
+}
+
+TEST_CASE("Comparing values from second")
+{
+  REQUIRE(result_2a == Approx(3.7564));
+
+  REQUIRE(result_2b == Approx(4.3323));
+}
+
+TEST_CASE("Comparing values from third")
+{
+  REQUIRE(expr1(x, y) == Approx(5.6091));
+
+  REQUIRE(expr2(x, y) == Approx(-8.9198));
 }
